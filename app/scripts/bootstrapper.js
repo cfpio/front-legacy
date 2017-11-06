caf47b4ba7440ef310893a5c94182d82fa87a3cc
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-11-06 19:33:39 +0100

    default API endpoint is same URL + /api

--- app/scripts/bootstrapper.js
+++ app/scripts/bootstrapper.js
@@ -24,7 +24,7 @@ No ng-app attribute in index.html. Config is retrieved from backend then Angular
 (function(window, document) {
 
     var config = {
-        apiBaseUrl: 'http://api.cfp.io/api',
+        apiBaseUrl: '/api',
         authServer: 'https://auth.cfp.io'
     };
     window.deferredBootstrapper.bootstrap({
